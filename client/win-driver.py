fd5c7c138c6efe036635d2f4fff614ac7429bc77
Frieder Keilholz <fgk1999@outlook.de>
2024-02-24 16:24:31 +0100

    Add meters.yml configuration file and update win-driver.py to use the metrics defined in meters.yml

--- client/win-driver.py
+++ client/win-driver.py
@@ -1,9 +1,34 @@
 import psutil
 import urllib.request
+import yaml.loader
+import time
+
+meters = yaml.safe_load(open('meters.yml'))
+
+def get_cpu_percent():
+    return str(int(psutil.cpu_percent()))
+
+def get_memory_percent():
+    return str(int(psutil.virtual_memory().percent))
+
+options = {
+    'cpu-percent': get_cpu_percent,
+    'memory-percent': get_memory_percent
+}
 
 while True:
-    #get cpu usage in percent over 0.5 seconds
-    util = str(int(psutil.cpu_percent(0.5)))
-    print(util)
-    #send get request to server
-    urllib.request.urlopen("http://192.168.178.172:80/util/" + util +"/")
+    for meter in meters['meters']:
+        #get corresponding metric function
+        util = options[meter['metric']]()
+        
+        #send get request to server
+        url = "http://" + meter['ip']+":"+str(meter['port'])+"/util"
+        print(url)
+        try:
+            urllib.request.urlopen(url + "/" + util +"/", timeout=0.5)
+        except TimeoutError:
+            print("TimeoutError")
+        except urllib.error.URLError:
+            print("URLError")
+    #await defined interval
+    time.sleep(meters['interval'])
